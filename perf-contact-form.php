f36f23cdd89f3ee719dd9787888844b510cc484b
Eric Valois <eric.v@bulledev.com>
2016-06-23 08:27:25 -0400

    Contact form cleaning

--- perf-contact-form.php
+++ perf-contact-form.php
@@ -67,7 +67,7 @@
  		$headers = "From: $name <$email>" . "\r\n";
 
  		$q = http_build_query(array(
- 	        'secret'    => '6LcS-xsTAAAAAO_29q1EINmcCAdbFtnY9XzKcKuc',
+ 	        'secret'    => get_field("perf_contact_private_key_copy","option"),
  	        'response'  => $_POST['g-recaptcha-response'],
  	        'remoteip'  => $_SERVER['REMOTE_ADDR'],
  		));
@@ -83,7 +83,6 @@
  		}
 
  		if ( $recaptcha && wp_mail( $to, $subject, $message, $headers ) ) {
- 			wp_mail( 'eric.v@bulledev.com', 'WP Mail Test', 'Mail is working' );
  			wp_mail( $to, $subject, $message, $headers );
 
  			echo '<div>';
